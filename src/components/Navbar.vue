e7348540c023d553f4f111d1962af93040e1bca7
James Blackman <JBlack262@west-mec.org>
2021-03-23 18:21:08 -0700

    content to homepage

---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="navbar">
+        <ul class="list">
+            <li><i @click="openNavbar" :class="{active: isActive}" class="fa fa-plus fa-3x"></i></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+        isActive: false,
+        }
+    },
+    methods: {
+        openNavbar() {
+            this.isActive = !this.isActive;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    body {
+        margin: none;
+    }
+    .navbar {
+        margin: none;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        background: skyblue;
+        ul.list {
+            list-style-type: none;
+            padding: 0 1rem;
+            margin: 0;
+            display: flex;
+            justify-content: space-between;
+                li {
+                    width: 100%;
+                    color: black;
+                    text-decoration: none;
+                    display: flex;
+                    align-items: center;
+                    justify-content: flex-end;
+                    cursor: pointer;
+                    i {
+                        float: right;
+                        padding-top: 2px;
+                    }
+                    .active {
+                        transform: rotate(90deg);
+                        transition: transform 800ms;
+                    }
+                }
+        }
+    }
+</style>
